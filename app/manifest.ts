ae490ab49c102499e383022d88343997ba6f669c
zen0904 <jj5w5k5v8f@privaterelay.appleid.com>
2026-08-04 01:30:50 +0800

    Add files via upload

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,7 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "landscape",
     icons: [
       { src: `${basePath}/icon-192.png`, sizes: "192x192", type: "image/png" },
-      { src: `${basePath}/icon-512.png`, sizes: "512x512", type: "image/png", purpose: "any maskable" },
+      { src: `${basePath}/icon-512.png`, sizes: "512x512", type: "image/png", purpose: "maskable" },
     ],
   };
 }
