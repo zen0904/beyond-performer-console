df9b0e8cdc440e48aff92c484dd4f91923ede485
zen0904 <jj5w5k5v8f@privaterelay.appleid.com>
2026-08-04 08:51:06 +0800

    Add files via upload

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   const basePath = process.env.GITHUB_ACTIONS ? "/beyond-performer-console" : "";
   return {
